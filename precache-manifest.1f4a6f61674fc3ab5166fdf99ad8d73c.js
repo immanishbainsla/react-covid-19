48a34f9916b77fed1346de597ab908fbccd10d3c
immanishbainsla <manishbainsla1072@gmail.com>
2020-08-27 18:42:41 +0530

    Updates

---
+++ precache-manifest.1f4a6f61674fc3ab5166fdf99ad8d73c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1631c910211d7e20e9410f0707b523",
+    "url": "/react-covid-19/index.html"
+  },
+  {
+    "revision": "456607366d198c65fbfd",
+    "url": "/react-covid-19/static/css/main.b16ce5e0.chunk.css"
+  },
+  {
+    "revision": "d1458d4d47e3d7619c6b",
+    "url": "/react-covid-19/static/js/2.6d8b2d07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-covid-19/static/js/2.6d8b2d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456607366d198c65fbfd",
+    "url": "/react-covid-19/static/js/main.1090b950.chunk.js"
+  },
+  {
+    "revision": "c1122267bb4f31d99193",
+    "url": "/react-covid-19/static/js/runtime-main.1435bc3d.js"
+  }
+]);
